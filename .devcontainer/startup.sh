c174bf0a5ffb62827f36b7ecf1095f1d05bdf48d
cmosier-LiL <88352515+cmosier-LiL@users.noreply.github.com>
2023-10-26 09:01:58 -0700

    Update startup.sh
    added code for setup postgres

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -3,3 +3,8 @@ apt install -y postgresql &&
 service postgresql start &&
 su postgres -c "psql -c 'create role vscode login;'" &&
 su postgres -c "psql -c 'create database linkedin;'"
+
+# Run an initial setup script for the MariaDB database
+if [ -f .devcontainer/setup-postgresql.sql ]; then
+  psql -h 127.0.0.1 -U postgres postgres < .devcontainer/setup-postgresql.sql
+fi
